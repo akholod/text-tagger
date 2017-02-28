0c2c2204ed25cf5cc8fa049401ebc744ffc1db34
akholod <andrey.kokhanevich@gmail.com>
2017-02-28 20:17:57 +0200

    complete app structure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-//const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -16,8 +15,6 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,7 +33,6 @@ app.listen(port, () => {
     console.log('App listen on port ' + port);
 });
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -45,7 +41,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 if (app.get('env') === 'development') {
     app.use((err, req, res) => {
